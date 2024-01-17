69323f27790471bffeafc41eefeb086538863797
HardScale <ericm301@gmail.com>
2024-01-17 10:59:00 -0800

    This pack really doesn't like pnpm.

--- packages/examples/example.ts
+++ packages/examples/example.ts
@@ -4,72 +4,75 @@ import { RingApi } from 'ring-client-api'
 import { readFile, writeFile } from 'fs'
 import { promisify } from 'util'
 
-async function getAuth() {
+function getAuth() {
   const { env } = process,
     ringApi = new RingApi({
       refreshToken: env.RING_REFRESH_TOKEN!,
       debug: true,
-    });
-  
-  const sub = ringApi.onRefreshTokenUpdated.subscribe(
-    async  ({oldRefreshToken, newRefreshToken}) => {
-      if (!oldRefreshToken) { return; }
+    }),
+    sub = ringApi.onRefreshTokenUpdated.subscribe(
+      async ({ oldRefreshToken, newRefreshToken }) => {
+        if (!oldRefreshToken) {
+          return
+        }
+        const currentConfig = await promisify(readFile)('.env'),
+          updatedConfig = currentConfig
+            .toString()
+            .replace(oldRefreshToken, newRefreshToken)
 
-      const currentConfig = await promisify(readFile)('.env'),
-        updatedConfig = currentConfig
-          .toString()
-          .replace(oldRefreshToken, newRefreshToken);
+        await promisify(writeFile)('.env', updatedConfig)
+      },
+    )
 
-      await promisify(writeFile)('.env', updatedConfig);
-    } 
-  )  
-
-  return  {ringApi, sub}
+  return { ringApi, sub }
 }
 
 function takeTime(timeval: number): string {
   // gets the time string and constructs a string like 'YYYYMMDD_HHmmss'
   const t = new Date(timeval),
     d2 = '2-digit',
-    DT_Opts: Intl.DateTimeFormatOptions = { 
-      year: "numeric", month: d2, day: d2,
-      hour: d2, minute: d2, second: d2      
-  }
-  const timeFormatter = Intl.DateTimeFormat('en-US', DT_Opts);
-  const timeFields: Intl.DateTimeFormatPart[] = timeFormatter.formatToParts(t);
-  const { year, month, day, hour, minute, second } = timeFields.reduce(
-    (obj, {type, value}) => {
-      if (type != 'literal') {
-        Object.defineProperty(obj, type, { value: value });
-      }
-      return obj;
-    },{}
-  ) as any;  // shut up, ts!
-  return `${year+month+day}_${hour+minute+second}`;  //  concatenation, NOT addition!
+    DT_Opts: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: d2,
+      day: d2,
+      hour: d2,
+      minute: d2,
+      second: d2,
+    },
+    timeFormatter = Intl.DateTimeFormat('en-US', DT_Opts),
+    timeFields: Intl.DateTimeFormatPart[] = timeFormatter.formatToParts(t),
+    { year, month, day, hour, minute, second } = timeFields.reduce(
+      (obj, { type, value }) => {
+        if (type !== 'literal') {
+          Object.defineProperty(obj, type, { value: value })
+        }
+        return obj
+      },
+      {},
+    ) as any // shut up, ts!
+  return `${year + month + day}_${hour + minute + second}` //  concatenation, NOT addition!
 }
 
 async function example() {
-  if (!process.env.RING_REFRESH_TOKEN) {
-    throw "Go get an access token and put it in the .env file."
-  }
+  const { ringApi, sub } = getAuth(),
+    // locations = await ringApi.getLocations(),
+    // home = locations[0],
+    cams = await ringApi.getCameras(),
+    fluffyCam = cams.find((cam) => cam.name === '@Fluffy')!
 
-  const { ringApi, sub } = await getAuth();
+  fluffyCam.requestUpdate()
 
-  const locations = await ringApi.getLocations(),
-        home = locations[0],
-        cams = ringApi.getCameras(),
-        fluffyCam = (await cams).find((cam) => cam.name == '@Fluffy' )!;
-  
-  fluffyCam.requestUpdate();
-  
   const snap = await fluffyCam.getSnapshot(),
-    snapAge = fluffyCam.currentTimestampAge;
+    snapAge = fluffyCam.currentTimestampAge
 
-  console.log(`\nSnap age: ${snapAge} seconds ago.\n`);
-  await promisify(writeFile)(`../../snap/${takeTime(Date.now() - 1000 * snapAge)}_snap.jpg`, snap)
-  
-  await sub.unsubscribe();
-  process.exit(0);
+  console.log(`\nSnap age: ${snapAge / 1000} seconds ago.\n`)
+  await promisify(writeFile)(
+    `../../snap/${takeTime(Date.now() - snapAge)}_snap.jpg`,
+    snap,
+  )
+
+  sub.unsubscribe()
+  process.exit(0)
 }
 
 example().catch((e: any) => {
